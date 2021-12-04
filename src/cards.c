4714cfdefcb9c6dc6f719671e52c965c5c1563d1
nate <natemitchell1256@gmail.com>
2021-12-04 04:12:37 +0000

    cards loading working

--- src/cards.c
+++ src/cards.c
@@ -4,9 +4,6 @@
 #include <ctype.h>
 #include <string.h>
 
-
-//https://www.geeksforgeeks.org/function-pointer-in-c/
-
 #define EFFECT_COUNT 2
 #define MAX_CARDS 6
 
@@ -28,6 +25,7 @@ typedef struct _Card {
     struct _Card *next;
 } Card;
 
+const char *name_of_var_for_print_f[] = {"cost_o", "cost_e", "cost_h", "targets", "dam_o", "dam_e", "dam_h"};
 Card *loadcards(const char *filename);
 void free_card(Card *next);
 
@@ -36,99 +34,120 @@ int main(void)
     Card *start = loadcards("data/testcards.txt");
     for (Card *current = start; current != NULL; current = current->next)
     {
-        printf("name:%s\n", current->name);
-        printf("dam:%i\n", current->dam_h);
+        printf("name: %s\n", current->name);
+        for (int i = 0; i < 7; i++)
+            printf("%s: %i\n", name_of_var_for_print_f[i], (&(current->cost_o))[i]);
+        for (int i = 0; i < EFFECT_COUNT; i++)
+        {
+            printf("effects %i: %i\n", i,  current->effects[i] ? 1 : 0);
+        }
+        printf("this %p -> next %p\n", current, current->next);
+        printf("\n");
+
     }
     free_card(start);
 }
 
-void free_card(Card *next)
+void free_card(Card *p)
 {
-    if (next->next != NULL)
+    //Recursively calls this function for every card in the list
+    if (p == NULL)
     {
-        free_card(next->next);
+        return;
     }
-    free(next->name);
-    free(next);
+    if (p->next != NULL)
+    {
+        free_card(p->next);
+    }
+
+    //Frees this card
+    if (p->name != NULL)
+    {
+        free(p->name);
+    }
+    free(p);
 }
 
 Card *loadcards(const char *filename)
 {
+    //Opens the given file
     FILE *file = fopen(filename, "r");
+    //Exits function if file does not exist
     if (file == NULL)
     {
         return 0;
     }
+    //Initializes all remaining variables to NULL
     Card *start = NULL;
-    char *buffer = malloc(50);
-    char words[9][sizeof(buffer)];
-    for (int i = 0; i < sizeof(buffer); i++)
-    {
-        buffer[i] = '\0';
-    }
+    Card *current;
+    char buffer;
+    char words[9][50];
     for (int i = 0; i < 9; i++)
     {
-        for (int j = 0; j < (int) sizeof(buffer); j++)
+        for (int j = 0; j <  50; j++)
         {
             words[i][j] = '\0';
         }
     }
-    while (fgets(buffer, sizeof(buffer), file))
-    {   
-        Card *current = malloc(sizeof(Card));
-        int j = 0;
-        int k = 0;
-        for (int i = 0; buffer[i]; i++)
+    //TODO: switch to fread
+    int j,k;
+    j = 0;
+    k = 0;
+    while (fread(&buffer, sizeof(char), 1, file))
+    {
+        if (buffer != '\n')
         {
             k++;
-            if (buffer[i] == ';')
+            if (buffer == ';')
             {
                 words[j][k - 1] = '\0';
-                k = 0;  
+                k = 0;
                 j++;
             }
             else
             {
-                words[j][k - 1] = buffer[i];
+                words[j][k - 1] = buffer;
             }
         }
-        current->name = malloc(sizeof(words[0]) + 1);
-        strcpy(current->name, &words[0][0]);
-        current->name[(int) sizeof(words[0])] = '\0';
-        for (int h = 0; h < 7; h++)
+        else
         {
-            for (int i = 0; words[h][i]; i++)
+            current = malloc(sizeof(Card));
+            current->name = malloc(strlen(words[0]) + 1);
+            strcpy(current->name, &words[0][0]);
+            current->name[strlen(words[0])] = '\0';
+            //Loops through the integer variable in the current carding, setting them in order
+            for (int h = 0; h < 7; h++)
             {
-                if (isdigit(words[h][i]))
+                for (int i = 0; words[h][i]; i++)
                 {
-                    (&(current->cost_o))[h] = atoi(words[h]);
+                    if (isdigit(words[h][i]))
+                    {
+                        //Sets the next variable in the card to the next word.
+                        (&(current->cost_o))[h]/*the h'th variable after the cost_o of current*/ = atoi(words[h]);
+                    }
                 }
             }
-        }
-        // cost_o   
-        // cost_e
-        // current->cost_h
-        // current->targets = atoi(words[4]);
-        // current->dam_o = atoi(words[5]);
-        // current->dam_e = atoi(words[6]);
-        // current->dam_h = atoi(words[7]);
-        for (int i = 0; i < EFFECT_COUNT; i++)
-        {
-            if (words[8][i] == '1')
+            //Scrolls through marking whether they are true or false for this card
+            for (int i = 0; i < EFFECT_COUNT; i++)
             {
-                current->effects[i] = true;
+                if (words[8][i] == '1')
+                {
+                    current->effects[i] = true;
+                }
+                else
+                {
+                    current->effects[i] = false;
+                }
             }
-            else
-            {
-                current->effects[i] = false;
-            }
+            //Places current at the front of the linked list
+            current->next = start;
+            start = current;
+            j = 0;
         }
-        current->next = start;
-        start = current;
     }
-
-    free(buffer);
+    //Frees buffer and file
     fclose(file);
+    //Returns the start of the linked list.
     return start;
 }
 
